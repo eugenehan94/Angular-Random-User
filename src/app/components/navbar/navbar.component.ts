8334d288a0074c1751b8c38400549656fe37a968
eugenehan94 <eugene.han94@gmail.com>
2022-09-18 15:54:00 -0400

    corrected width, adding comments

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
-import {
-  Component,
-  OnInit,
-  HostListener,
-} from '@angular/core';
+// @TODO: When client is on specific route, clicking it from mobile menu,
+// should allow anything to happen. Which includes the menu shouldn't close.
+
+import { Component, OnInit, HostListener } from '@angular/core';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd, NavigationStart, Event as NavigationEvent } from '@angular/router';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -15,17 +15,47 @@ export class NavbarComponent implements OnInit {
   faXmark = faXmark;
   showMobileMenu: boolean = false;
 
+  currentPageMobileMenu?: string;
+
+  routerEvent?:any;
   public screenWidth!: number;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, location: Location) {}
 
   ngOnInit(): void {
-    this.screenWidth = window.innerWidth;
+   this.routerEvent= this.screenWidth = window.innerWidth;
+    // This block of code reads the current page user is on
+    // example: /link-four
+    this.router.events.subscribe((event:NavigationEvent) => {
+      if (event instanceof NavigationStart){
+        console.log("event.url: ", event.url.substring(1).replace('-', ' ') )
+        this.currentPageMobileMenu = event.url.substring(1).replace('-', ' ')
+      }
+    })
+
+    // console.log(this.router.events);
+    // console.log('window.location.href: ', window.location.href);
+
+    // this.router.events.subscribe((val) => {
+    //   if (location.path() != '') {
+    //     console.log("location: ", location.path())
+    //     // this.route = location.path();
+    //   } else {
+    //     console.log("else executed")
+    //     // this.route = 'Home';
+    //   }
+    // });
   }
 
+  ngOnDestroy() {
+    // Unsubscribe to the subscribe router events
+    this.routerEvent.unsubscribe();
+  }
+  // Toggles the open/close state of the Mobile menu
   toggleMobileMenu(): void {
     this.showMobileMenu = !this.showMobileMenu;
   }
+  // Triggers when window is being resized, the value is then stored in "screenWidth"
   @HostListener('window:resize', ['$event'])
   onWindowResize() {
     this.screenWidth = window.innerWidth;
