79b8c3d1269a182677f38a680d4f6bfbfeb75a6f
eugenehan94 <eugene.han94@gmail.com>
2022-09-19 20:17:19 -0400

    added mobile menu footer, planning to design card

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,6 @@
-// @TODO: When client is on specific route, clicking it from mobile menu,
-// should allow anything to happen. Which includes the menu shouldn't close.
-
 import { Component, OnInit, HostListener } from '@angular/core';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
-import { Router, NavigationEnd, NavigationStart, Event as NavigationEvent } from '@angular/router';
+import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 @Component({
   selector: 'app-navbar',
@@ -17,34 +14,15 @@ export class NavbarComponent implements OnInit {
 
   currentPageMobileMenu?: string;
 
-  routerEvent?:any;
+  routerEvent?: any;
   public screenWidth!: number;
+
+  year: number = new Date().getFullYear();
 
   constructor(private router: Router, location: Location) {}
 
   ngOnInit(): void {
-   this.routerEvent= this.screenWidth = window.innerWidth;
-    // This block of code reads the current page user is on
-    // example: /link-four
-    this.router.events.subscribe((event:NavigationEvent) => {
-      if (event instanceof NavigationStart){
-        console.log("event.url: ", event.url.substring(1).replace('-', ' ') )
-        this.currentPageMobileMenu = event.url.substring(1).replace('-', ' ')
-      }
-    })
-
-    // console.log(this.router.events);
-    // console.log('window.location.href: ', window.location.href);
-
-    // this.router.events.subscribe((val) => {
-    //   if (location.path() != '') {
-    //     console.log("location: ", location.path())
-    //     // this.route = location.path();
-    //   } else {
-    //     console.log("else executed")
-    //     // this.route = 'Home';
-    //   }
-    // });
+    this.screenWidth = window.innerWidth;
   }
 
   ngOnDestroy() {
@@ -52,12 +30,21 @@ export class NavbarComponent implements OnInit {
     this.routerEvent.unsubscribe();
   }
   // Toggles the open/close state of the Mobile menu
-  toggleMobileMenu(): void {
+  toggleMobileMenu(input?: any): void {
     this.showMobileMenu = !this.showMobileMenu;
   }
+
   // Triggers when window is being resized, the value is then stored in "screenWidth"
   @HostListener('window:resize', ['$event'])
   onWindowResize() {
     this.screenWidth = window.innerWidth;
+  }
+
+  openPersonalWebsite(): void {
+    window.open(
+      'https://eugenehan.netlify.app/',
+      '_blank',
+      'noopener, noreferrer'
+    );
   }
 }
