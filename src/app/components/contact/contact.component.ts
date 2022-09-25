11287da7ee2ba6b7a8248d785045501c47b9a8e9
eugenehan94 <eugene.han94@gmail.com>
2022-09-24 21:20:31 -0400

    added some reactive form

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from "@angular/forms"
+import { FormGroup, FormControl } from "@angular/forms"
+import { FormBuilder } from "@angular/forms";
+import { Validators } from "@angular/forms";
 import {
   faFacebookF,
   faInstagram,
@@ -15,9 +17,26 @@ export class ContactComponent implements OnInit {
   faInstagram = faInstagram;
   faTwitter = faTwitter;
 
-  name = new FormControl('');
+  // profileForm = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  //   email: new FormControl(''),
+  // })
 
-  constructor() {}
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.required]
+  })
 
-  ngOnInit(): void {}
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    console.log("profileForm: ", this.profileForm)
+  }
+
+  onSubmit(){
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value)
+  }
 }
